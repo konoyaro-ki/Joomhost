ca20b84e080192da148ef9c28537691560d0403e
konoyaro-ki <chepel1997@gmail.com>
2022-05-22 18:11:22 +0600

    .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
   $('.reviews-slider').slick({
     Infinity: true,
@@ -6,9 +6,19 @@ $(function(){
     slidesToScroll: 2,
     arrows: false,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1106,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+
+        }
+      },
+    ]
   });
 
-  $('.btn__menu').on('click', function(){
+  $('.btn__menu').on('click', function () {
     $('.menu__list').slideToggle();
   });
 
